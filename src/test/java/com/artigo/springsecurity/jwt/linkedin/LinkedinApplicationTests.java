7b74a6391a7941a130efc233107016a0505824e3
RicardoBarrote <ricardoambscfc@gmail.com>
2025-02-05 17:56:18 -0300

    Iniciando o projeto

---
+++ src/test/java/com/artigo/springsecurity/jwt/linkedin/LinkedinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artigo.springsecurity.jwt.linkedin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkedinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
