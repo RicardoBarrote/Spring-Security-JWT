763eaf8d23e6231a6e1e4edcf6f9748d8bb683a2
RicardoBarrote <ricardoambscfc@gmail.com>
2025-02-06 14:29:01 -0300

    Exceção personalizada ClienteNotFoundException

---
+++ src/main/java/com/artigo/springsecurity/jwt/linkedin/backend/cliente/exception/ClienteNotFoundException.java
@@ -0,0 +1,8 @@
+package com.artigo.springsecurity.jwt.linkedin.backend.cliente.exception;
+
+public class ClienteNotFoundException extends RuntimeException {
+
+    public ClienteNotFoundException(String msg) {
+    }
+
+}
