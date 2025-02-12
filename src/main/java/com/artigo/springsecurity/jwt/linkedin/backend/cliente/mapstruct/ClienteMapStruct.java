15387e36280a39bfaef06ee6dd3a22a2ba66bf73
RicardoBarrote <ricardoambscfc@gmail.com>
2025-02-12 20:54:30 -0300

    Dependencia do MapStruct

---
+++ src/main/java/com/artigo/springsecurity/jwt/linkedin/backend/cliente/mapstruct/ClienteMapStruct.java
@@ -0,0 +1,18 @@
+package com.artigo.springsecurity.jwt.linkedin.backend.cliente.mapstruct;
+
+import com.artigo.springsecurity.jwt.linkedin.backend.cliente.Cliente;
+import com.artigo.springsecurity.jwt.linkedin.backend.cliente.dto.ClienteResponseDto;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.ReportingPolicy;
+
+import java.util.List;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING
+        , unmappedTargetPolicy = ReportingPolicy.ERROR)
+public interface ClienteMapStruct {
+
+    ClienteResponseDto converterParaResponseDto(Cliente cliente);
+
+    List<ClienteResponseDto> converterListaParaResponseDto(List<Cliente> clienteList);
+}
