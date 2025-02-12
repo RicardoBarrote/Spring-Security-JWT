cb1570366d3e504d5634cb5ab85f5c6bfb7882bb
RicardoBarrote <ricardoambscfc@gmail.com>
2025-02-12 01:00:30 -0300

    Class JwtTokenService, gerar e validar token JWT

--- src/main/java/com/artigo/springsecurity/jwt/linkedin/backend/infra/configuration/security/ConfigurationSecurity.java
+++ src/main/java/com/artigo/springsecurity/jwt/linkedin/backend/infra/configuration/security/ConfigurationSecurity.java
@@ -7,24 +7,36 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class ConfigurationSecurity {
 
+    private final filtroDeSeguranca filtroDeSeguranca;
+
+    public ConfigurationSecurity(com.artigo.springsecurity.jwt.linkedin.backend.configuration.security.filtroDeSeguranca filtroDeSeguranca) {
+        this.filtroDeSeguranca = filtroDeSeguranca;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity security) throws Exception {
         return security.csrf(
-                        c -> c.disable()
+                        AbstractHttpConfigurer::disable
                 ).sessionManagement(s ->
                         s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(a ->
                         a.requestMatchers(HttpMethod.POST, "/clientes").permitAll()
-                                .anyRequest().permitAll())
+                                .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/clientes").hasRole("CLIENTE")
+                                .anyRequest().authenticated()
+                )
+                .addFilterBefore(filtroDeSeguranca, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
